e1f5681dd50a48671375ae0d229464ba624acedb
Stephan Krauss <develop@stephankrauss.de>
2016-08-11 14:00:40 +0200

    Verbessern Vers 0.1

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <html>
 <head>
-    <title>Stundenplände der Oberschule Bergstadt Schneeberg</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+    <title>Stundenplände der Oberschule Bergstadt Schneeberg</title>
+
+    <!-- CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Javascript -->
+    <script type="text/javascript" src="jquery/jquery-1.11.3.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/htm\14 l5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respo\16 nd.min.js"></script>
+    <![endif]-->
 
     <style>
         a {
@@ -21,6 +37,8 @@
     </style>
 </head>
 <body>
+
+
 
 <img src="icon_schule_mini.png">
 
