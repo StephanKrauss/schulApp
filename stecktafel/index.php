16432a98a68f2582ba6951217e4c65de0f0803ad
Stephan Krauss <develop@stephankrauss.de>
2016-08-29 10:40:51 +0200

    Test Stecktafel

---
+++ stecktafel/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+
+    <title>Stecktafel der Oberschule Bergstadt Schneeberg</title>
+
+    <!-- CSS -->
+    <link href="/_public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/_public/bootstrap/css/theme.min.css" rel="stylesheet">
+    <link href="/_public/css/main.css" rel="stylesheet">
+	
+	<style>
+		body{
+			background-image:url('schul_logo.jpg')
+		}
+	</style>
+
+    <!-- Javascript -->
+    <script type="text/javascript" src="/_public/jquery/jquery-1.11.3.js"></script>
+    <script type="text/javascript" src="/_public/bootstrap/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/htm\14 l5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respo\16 nd.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	
+</body>
+</html>
